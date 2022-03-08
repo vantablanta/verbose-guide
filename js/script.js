bcb8bcd17b624eb1528731e86b1f862aff8c366a
vanta blanta <vantablanta@gmail.com>
2022-03-08 14:58:48 +0300

    jQuery() shortened to

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-jQuery(document).ready(function(){
+$(document).ready(function(){
 
-jQuery('h1').click(function(){
+$('h1').click(function(){
     alert("This is a heading tag")
 });
 
-jQuery('p').click(function(){
+$('p').click(function(){
     alert("This is a paragraph")
 });
 
-jQuery('img').click(function(){
+$('img').click(function(){
     alert("This is an image")
 });
 
