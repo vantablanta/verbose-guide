3fe6afc9f996a5fcb95fe54a654618f1307fe872
vanta blanta <vantablanta@gmail.com>
2022-03-08 14:53:25 +0300

    jquery cdn added, jquery functions used in script.js documnt.ready called to allow the DOM to load first

---
+++ js/script.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function(){
+
+jQuery('h1').click(function(){
+    alert("This is a heading tag")
+});
+
+jQuery('p').click(function(){
+    alert("This is a paragraph")
+});
+
+jQuery('img').click(function(){
+    alert("This is an image")
+});
+
+
+})
+
